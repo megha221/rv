2493a7619ef0dc6aba076a86ed603ab5775d307d
meghana <meghana@meghanas-MacBook-Air.local>
2024-11-20 23:04:42 +0530

    aa

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,216 +1,46 @@
-$(function () {
-  "use strict";
-  $('[data-toggle="tooltip"]').tooltip();
-});
+// slick slider
+$(".slick-slider").slick({
+  slidesToShow: 2,
+  infinite:true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000
+ });
 
-$(window).on("load", function () {
-  var delayMs = 10000; // delay in milliseconds
 
-  setTimeout(function () {
-    $("#enquire-modal").modal("show");
-  }, delayMs);
-});
+ var is_mobile = 'No';
 
-$(document).ready(function () {
-  //code for Whatsapp message
-  let urlString = window.location.href;
-  console.log(urlString);
-  $(document).ready(function () {
-    const url = new URL(urlString);
-    $(".website_url").val(url.href);
-    const utm_source = url.searchParams.get("utm_source");
-    const mainsource = url.searchParams.get("mainsource");
-
-    // Check if the URL parameter is from utm_source
-    if (utm_source) {
-      // Check if the URL parameter is Google or google
-      if (utm_source.toLowerCase() === "google") {
-        
-        var whatsappBtnDesktop = document.getElementById("discovery");
-        if (whatsappBtnDesktop) {
-          whatsappBtnDesktop.parentNode.removeChild(whatsappBtnDesktop);
-        }
-        var whatsappBtnMobile = document.getElementById("discovery_mobile");
-        if (whatsappBtnMobile) {
-          whatsappBtnMobile.parentNode.removeChild(whatsappBtnMobile);
-        }
-      } else if (utm_source.toLowerCase() === "ppc") {
-        document.getElementById("discovery").href =
-          "https://api.whatsapp.com/send?phone=917304824642&text=Hi%20I'm%20interested%20in%20Learning%20more%20About%20Brigade%20Sanctuary%20At%20Whitefield-Sarjapur%20Road.%20Please%20Share%20Details.";
-        document.getElementById("discovery_mobile").href =
-          "https://api.whatsapp.com/send?phone=917304824642&text=Hi%20I'm%20interested%20in%20Learning%20more%20about%20Brigade%20Sanctuary%20At%20Whitefield-Sarjapur%20Road.%20Please%20Share%20Details.";
-      }
-    } else if (mainsource) {
-      // Check if the URL parameter is from mainsource
-      // Check if the URL parameter is GoogleY, GoogleA, etc.
-      if (mainsource.startsWith("Google")) {
-        // document.getElementById('discovery').href = "https://api.whatsapp.com/send?phone=917304824642&text=Hello%2C%20I%20would%20like%20to%20explore%20further%20details%20about%20Brigade%20Sanctuary%20-%20Whitefield-Sarjapur%20Road.";
-        // document.getElementById('discovery_mobile').href = "https://api.whatsapp.com/send?phone=917304824642&text=Hello%2C%20I%20would%20like%20to%20explore%20further%20details%20about%20Brigade%20Sanctuary%20-%20Whitefield-Sarjapur%20Road.";
-        var whatsappBtnDesktop = document.getElementById("discovery");
-        if (whatsappBtnDesktop) {
-          whatsappBtnDesktop.parentNode.removeChild(whatsappBtnDesktop);
-        }
-        var whatsappBtnMobile = document.getElementById("discovery_mobile");
-        if (whatsappBtnMobile) {
-          whatsappBtnMobile.parentNode.removeChild(whatsappBtnMobile);
-        }
-      } else if (mainsource.toLowerCase() === "ppc") {
-        document.getElementById("discovery").href =
-          "https://api.whatsapp.com/send?phone=917304824642&text=Hi%20I'm%20interested%20in%20Learning%20more%20About%20Brigade%20Sanctuary%20At%20Whitefield-Sarjapur%20Road.%20Please%20Share%20Details.";
-        document.getElementById("discovery_mobile").href =
-          "https://api.whatsapp.com/send?phone=917304824642&text=Hi%20I'm%20interested%20in%20Learning%20more%20about%20Brigade%20Sanctuary%20At%20Whitefield-Sarjapur%20Road.%20Please%20Share%20Details.";
-      }
-    }
-  });
-
-  /////form name submission code//////////
-  $(".custom-btn").click(function () {
-    var myBookId = $(this).data("id");
-    $(".form_name").val(myBookId);
-    //document.getElementsByName('form_name').value = myBookId;
-  });
-
-  $(".data-id-btn").click(function () {
-    var myBookId = $(this).data("id");
-    $(".form_name").val(myBookId);
-    //document.getElementsByName('form_name').value = myBookId;
-  });
-
-  // code for button more and less
-  $(".moreBtn").click(function () {
-    if ($(this).html() === 'Read more <i class="fa fa-chevron-down"></i>') {
-      $(this).html('Read less <i class="fa fa-chevron-up"></i>');
-    } else {
-      $(this).html('Read more <i class="fa fa-chevron-down"></i>');
-    }
-    $(".moreText[data-hit=more" + $(this).data("target") + "]").slideToggle(
-      500
-    );
-  });
-
-  $(".moredisclaimerBtn").click(function () {
-    if ($(this).html() === 'Read more <i class="fa fa-chevron-down"></i>') {
-      $(this).html('Read less <i class="fa fa-chevron-up"></i>');
-    } else {
-      $(this).html('Read more <i class="fa fa-chevron-down"></i>');
-    }
-    $(
-      ".moredisclaimerText[data-hit=more" + $(this).data("target") + "]"
-    ).slideToggle(500);
-  });
-
-  var exampleModal = document.getElementById("enquire-modal");
-  exampleModal.addEventListener("show.bs.modal", function (event) {
-   
-    var button = event.relatedTarget;
-    
-    var recipient = button.getAttribute("data-bs-whatever");
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      is_mobile = 'Yes';
+      $(".slick-slider1, .slick-slider11").slick({
+          slidesToShow: 1,
+          infinite:true,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000
+         });
+  }
  
-    var modalTitle = exampleModal.querySelector(".modal-title");
-
-    modalTitle.textContent = recipient;
-    modalBodyInput.value = recipient;
-  });
-
-  if ($(window).innerWidth() < 576) {
-    clearInterval(formInterval);
+  if(is_mobile=='No')
+  {
+      $(".slick-slider1, .slick-slider11").slick({
+          slidesToShow: 3,
+          infinite:true,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000
+         });
   }
 
-  $(".num").on("keyup", function (e) {
-    if (this.value != "") {
-      $(".check").prop("checked", "checked");
-    } else {
-      $(".check").prop("checked", "");
-    }
-  });
 
-  // Popup youtube JS
-  $(".popup-youtube").magnificPopup({
-    disableOn: 320,
-    type: "iframe",
-    mainClass: "mfp-fade",
-    removalDelay: 160,
-    preloader: false,
+//    // slick slider gallery
 
-    fixedContentPos: false,
-  });
-});
 
-///form submit///
-function submitForm(event, formName) {
-  event.preventDefault(); // Prevent default form submission
-
-  // Get form data
-  var formData = new FormData(document.forms[formName]);
-  var currentUrl = window.location.href; // Capture current URL
-
-  // Clear previous errors
-  var errorMessages = document.querySelectorAll(".error");
-  errorMessages.forEach(function (error) {
-    error.style.display = "none";
-  });
-
-  // Get name and phone values
-  var nameValue = formData.get("name");
-  var phoneValue = formData.get("mobile");
-  var formnameValue = formData.get("form_name");
-
-  // Validate name and phone
-  // var nameError = document.querySelectorAll('.error')[0];
-  // var phoneError = document.querySelectorAll('.error')[1];
-  var nameError = document.forms[formName].querySelectorAll(".error")[0];
-  var phoneError = document.forms[formName].querySelectorAll(".error")[1];
-
-  // If both fields are empty, display both error messages
-  if (!nameValue && !phoneValue) {
-    nameError.style.display = "block";
-    phoneError.style.display = "block";
-    return;
-  }
-  // Validate name and phone
-  if (!nameValue) {
-    nameError.style.display = "block";
-    document.querySelectorAll(".error")[0].style.display = "block";
-    return; // Exit function if name is empty
-  }
-
-  if (!phoneValue) {
-    phoneError.style.display = "block";
-    return; // Exit function if phone is empty
-  }
-
-  var form_data = {
-    name: formData.get("name"),
-    email: formData.get("email"),
-    mobile: formData.get("mobile"),
-    mobile: formData.get("mobile"),
-    form_name: formData.get("form_name"),
-    website_url: formData.get("website_url"),
-    currentUrl,
-  };
-
-  // Add current URL to form data
-  // Add current URL to form data
-  formData += "&currentUrl=" + encodeURIComponent(currentUrl);
-
-  // AJAX request
-  $.ajax({
-    type: "POST",
-    url: "send_email.php",
-    data: form_data,
-
-    success: function (response) {
-      if (formnameValue === "Brochure") {
-        // window.location.href = 'thank-you?formName=Brochure'; // Redirect to xyx.html
-        window.location.href = "thank-you.html";
-      } else {
-        window.location.href = "thank-you.html"; // Redirect to thank-you.html
-      }
-    },
-    error: function (xhr, status, error) {
-      // 			alert('Error occurred. Status: ' + xhr.status);
-      window.location.href = "thank-you.html";
-    },
-  });
-}
-
+//       // slick slider amenities
+// $(".slick-slider11").slick({
+//     slidesToShow: 3,
+//     infinite:true,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000
+//    });
